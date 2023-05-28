80f7d1a718030377954bb082298608221ec71b8a
hasan-huda <hasanhuda037@gmail.com>
2023-05-27 22:52:11 -0400

    first commit

---
+++ main.py
@@ -0,0 +1,12 @@
+import prettytable
+
+from prettytable import PrettyTable
+
+table = PrettyTable()
+
+table.add_column("Pokemon Name", ["Pickachu", "Squirtle", "Charmander"])
+table.add_column("Type", ["Electric", "Water", "Fire"])
+
+table.align = "l"
+
+print(table)
